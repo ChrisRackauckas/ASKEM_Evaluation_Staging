46a9ca06e676387c071d48892a449d2b3de62c4e
anand jain <anandj@uchicago.edu>
2023-01-30 15:10:26 -0500

    small changers

--- Scenario2/bin.jl
+++ Scenario2/bin.jl
@@ -7,7 +7,6 @@
 # To reproduce Fig 3d, set Event_trigger_Fig3b = 0, Event_trigger_Fig3d = 1, Event_trigger_Fig4b = 0, Event_trigger_Fig4d = 0, epsilon_modifer = 1 and run for t = 350 days.
 # To reproduce Fig 4b, set Event_trigger_Fig3b = 0, Event_trigger_Fig3d = 0, Event_trigger_Fig4b = 1, Event_trigger_Fig4d = 0, epsilon_modifer = 0 and run for t = 350 days.
 # To reproduce Fig 4d, set Event_trigger_Fig3b = 0, Event_trigger_Fig3d = 0, Event_trigger_Fig4b = 0, Event_trigger_Fig4d = 1, epsilon_modifer = 0 and run for t = 350 days.
-
 using OrdinaryDiffEq, ModelingToolkit, EasyModelAnalysis, SBML, SBMLToolkit, UnPack, Test
 import Plots
 
@@ -32,6 +31,10 @@ defs_ = ModelingToolkit.defaults(sys)
 defs = deepcopy(defs_)
 evs = ModelingToolkit.get_continuous_events(sys)
 
+@unpack Infected, Healed, Extinct, Diagnosed, Ailing, Recognized, Susceptible, Threatened = sys
+@unpack alpha, epsilon, gamma, beta, delta, mu, nu, lambda, rho, kappa, xi, sigma, zeta, eta, theta, tau = sys
+ps = [alpha, epsilon, gamma, beta, delta, mu, nu, lambda, rho, kappa, xi, sigma, zeta, eta]
+
 @parameters t
 D = Differential(t)
 eqs2 = deepcopy(eqs)
@@ -46,24 +49,51 @@ sol = solve(prob, Tsit5())
 @test sol.retcode == ReturnCode.Success
 Plots.plot(sol)
 
-sys = structural_simplify(odesys)
-@unpack Infected, Exposed, Deceased, Recovered, Total_population, Susceptible = sys
-
-pbounds = [
-    alpha => [0.003, 0.006],
-    epsilon => [1 / 6, 1 / 2],
-    gamma => [0.1, 0.2],
-    mu => [0.01, 0.02],
-    beta => [0.7, 0.9],
-]
-create_sensitivity_plot(prob, 100.0, Deceased, pbounds; samples = 2000)
-
 # Unit Test #1:
-u0s = [I => 200/60e6, D => 20/60e6, A => 1/60e6, R => 2/60e6, T => 0, H => 0, E => 0; S => 1]
-pars = [alpha => 0.570, beta => 0.011, delta = 0.011, gamma => 0.456, epsilon => 0.171, theta => 0.371,
-        zeta => 0.125, eta => 0.125, mu => 0.017, nu => 0.027, tau => 0.01,
-        lambda => 0.034, rho => 0.034,  kappa => 0.017,  xi => 0.017,  sigma => 0.017] # The resulting basic reproduction number is R0 = 2.38.
+ITALY_POPULATION = 60e6
+u0s = [
+    Infected => 200 / ITALY_POPULATION,
+    Diagnosed => 20 / ITALY_POPULATION,
+    Ailing => 1 / ITALY_POPULATION,
+    Recognized => 2 / ITALY_POPULATION,
+    Threatened => 0,
+    Healed => 0,
+    Extinct => 0,
+]
+push!(u0s, Susceptible => 1 - sum(last.(u0s)))
+
+# The resulting basic reproduction number is R0 = 2.38.
+pars = [alpha => 0.570, beta => 0.011, delta => 0.011, gamma => 0.456, epsilon => 0.171,
+    theta => 0.371,
+    zeta => 0.125, eta => 0.125, mu => 0.017, nu => 0.027, tau => 0.01,
+    lambda => 0.034, rho => 0.034, kappa => 0.017, xi => 0.017, sigma => 0.017]
 
 prob_test1 = remake(prob, u0 = u0s, p = pars)
-# maybe we should allow get_max_t to take a term and have it generate an observable 
-xmax, xmaxval = get_max_t(prob_test1, sum([I, D, A, R, T]))
+solt1 = solve(prob_test1, Tsit5(); saveat = 0:100)
+idart = [Infected, Diagnosed, Ailing, Recognized, Threatened]
+
+@test solt1[Infected + Healed] == solt1[Infected] + solt1[Healed]
+plot(solt1.t, solt1[sum(idart)] * ITALY_POPULATION;label="IDART absolute")
+plot(solt1.t, solt1[sum(idart)];label="IDART percent")
+
+xmax, xmaxval = get_max_t(prob_test1, sum(idart))
+
+@test isapprox(xmax, 47; atol = 5)
+@test isapprox(xmaxval, 0.6)
+
+# Unit test #2:
+# To reproduce Fig 2b and 2d, set Event_trigger_Fig3b = 0, Event_trigger_Fig3d = 0, Event_trigger_Fig4b = 0, Event_trigger_Fig4d = 0, epsilon_modifer = 1, alpha_modifer = 1 and run for t = 45 d
+# -> These are already the settings in the Model
+
+sol = solve(prob, Tsit5(), tspan = (0.0, 45.0))
+
+# @unpack Infected, Extinct = sys
+
+# pbounds = [
+#     alpha => [0.003, 0.006],
+#     epsilon => [1 / 6, 1 / 2],
+#     gamma => [0.1, 0.2],
+#     mu => [0.01, 0.02],
+#     beta => [0.7, 0.9],
+# ]
+# create_sensitivity_plot(prob, 100.0, Extinct, pbounds; samples = 20)
