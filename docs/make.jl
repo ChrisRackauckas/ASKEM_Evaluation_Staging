0391514f801a226457f9871369f1a868b59a911d
Chris Rackauckas <accounts@chrisrackauckas.com>
2023-01-31 02:57:47 -0500

    typo fix

--- docs/make.jl
+++ docs/make.jl
@@ -42,7 +42,7 @@ makedocs(sitename = "DARPA-ASKEM Evalution",
              "DARPA-ASKEM Evalution" => "index.md",
              #"Scenario1/Evaluation_Scenario_1.md",
              #"Scenario2/Evalution_Scenario_2.md",
-             "Scenario3/Evalution_Scenario_3.md",
+             "Scenario3/Evaluation_Scenario_3.md",
          ])
 
 deploydocs(repo = "github.com/ChrisRackauckas/ASKEM_Evaluation_Staging.jl")
